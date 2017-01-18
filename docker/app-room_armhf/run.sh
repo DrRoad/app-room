462542b7a2deac7c6e4757d0b3317c0908cc480e
Christoph Fabianek <christoph.fabianek@gmail.com>
2017-01-18 11:12:49 +0100

    docker armhf cleanup

--- docker/app-room_armhf/run.sh
+++
@@ -1,3 +0,0 @@
-docker stop raumklima
-docker rm $(docker ps -q -f status=exited)
-docker run --name raumklima -d -p 3838:3838 oydeu/app-room_armhf
--- docker/app-room_armhf/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -eq 0 ]
-  then
-    docker build -t oydeu/app-room_armhf .
-  else
-    docker build --no-cache -t oydeu/app-room_armhf .
-fi
-docker push oydeu/app-room_armhf
